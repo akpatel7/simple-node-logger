1941929924c88dda1b88219559c39052b5818aee
Amit Patel <akpatel7@users.noreply.github.com>
2022-05-23 15:26:30 -0400

    restored parsable log file format

--- src/config/morganMiddleware.ts
+++ src/config/morganMiddleware.ts
@@ -1,35 +1,67 @@
-import morgan, { StreamOptions } from "morgan";
-
+// import morgan, { StreamOptions } from "morgan";
+import morgan from "morgan";
+import json from 'morgan-json';
 import Logger from "../libs/logger";
 
-// Override the stream method by telling
-// Morgan to use our custom logger instead of the console.log.
-const stream: StreamOptions = {
-    // Use the http severity
-    write: (message) => Logger.http(message),
-};
+// // Override the stream method by telling
+// // Morgan to use our custom logger instead of the console.log.
+// const stream: StreamOptions = {
+//     // Use the http severity
+//     write: (message) => Logger.http(message),
+// };
 
-// Skip all the Morgan http log if the
-// application is not running in development mode.
-// This method is not really needed here since
-// we already told to the logger that it should print
-// only warning and error messages in production.
-const skip = () => {
-    // eslint-disable-next-line no-undef
-    const env = process.env.NODE_ENV || "development";
-    return env !== "development";
-};
+// // Skip all the Morgan http log if the
+// // application is not running in development mode.
+// // This method is not really needed here since
+// // we already told to the logger that it should print
+// // only warning and error messages in production.
+// const skip = () => {
+//     // eslint-disable-next-line no-undef
+//     const env = process.env.NODE_ENV || "development";
+//     return env !== "development";
+// };
 
-// Build the morgan middleware
-const morganMiddleware = morgan(
-    // Define message format string (this is the default one).
-    // The message format is made from tokens, and each token is
-    // defined inside the Morgan library.
-    // You can create your custom token to show what do you want from a request.
-    ":method :url :status :res[content-length] - :response-time ms",
-    // Options: in this case, I overwrote the stream and the skip logic.
-    // See the methods above.
-    { stream, skip }
-);
+// // Build the morgan middleware
+// const morganMiddleware = morgan(
+//     // Define message format string (this is the default one).
+//     // The message format is made from tokens, and each token is
+//     // defined inside the Morgan library.
+//     // You can create your custom token to show what do you want from a request.
+//     ":method :url :status :res[content-length] - :response-time ms",
+//     // Options: in this case, I overwrote the stream and the skip logic.
+//     // See the methods above.
+//     { stream, skip }
+// );
+
+const format = json({
+    method: ':method',
+    url: ':url',
+    status: ':status',
+    contentLength: ':res[content-length]',
+    responseTime: ':response-time'
+})
+
+const morganMiddleware = morgan(format, {
+    stream: {
+        write: (message) => {
+            const {
+                method,
+                url,
+                status,
+                contentLength,
+                responseTime
+            } = JSON.parse(message)
+
+            Logger.info('HTTP Access Log', {
+                timestamp: new Date().toString(),
+                method,
+                url,
+                status: Number(status),
+                contentLength,
+                responseTime: Number(responseTime)
+            })
+        }
+    }
+})
 
 export default morganMiddleware;
--- src/libs/logger.js
+++ src/libs/logger.js
@@ -1,52 +1,79 @@
 import winston from 'winston'
 
-const levels = {
-    error: 0,
-    warn: 1,
-    info: 2,
-    http: 3,
-    debug: 4,
-}
+// const levels = {
+//     error: 0,
+//     warn: 1,
+//     info: 2,
+//     http: 3,
+//     debug: 4,
+// }
 
-const level = () => {
-    // eslint-disable-next-line no-undef
-    const env = process.env.NODE_ENV || 'development'
-    const isDevelopment = env === 'development'
-    return isDevelopment ? 'debug' : 'warn'
-}
+// const level = () => {
+//     // eslint-disable-next-line no-undef
+//     const env = process.env.NODE_ENV || 'development'
+//     const isDevelopment = env === 'development'
+//     return isDevelopment ? 'debug' : 'warn'
+// }
 
-const colors = {
-    error: 'red',
-    warn: 'yellow',
-    info: 'green',
-    http: 'magenta',
-    debug: 'white',
-}
+// const colors = {
+//     error: 'red',
+//     warn: 'yellow',
+//     info: 'green',
+//     http: 'magenta',
+//     debug: 'white',
+// }
 
-winston.addColors(colors)
+// winston.addColors(colors)
 
-const format = winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-    winston.format.colorize({ all: true }),
-    winston.format.printf(
-        (info) => `${info.timestamp} ${info.level}: ${info.message}`,
-    ),
-)
+// const format = winston.format.combine(
+//     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+//     winston.format.colorize({ all: true }),
+//     winston.format.printf(
+//         (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+//     ),
+// )
 
-const transports = [
-    new winston.transports.Console(),
-    new winston.transports.File({
-        filename: 'logs/error.log',
-        level: 'error',
-    }),
-    new winston.transports.File({ filename: './logs/all.log' }),
-]
+// const transports = [
+//     new winston.transports.Console(),
+//     new winston.transports.File({
+//         filename: 'logs/error.log',
+//         level: 'error',
+//     }),
+//     new winston.transports.File({ filename: './logs/all.log' }),
+// ]
+
+// const Logger = winston.createLogger({
+//     level: level(),
+//     levels,
+//     format,
+//     transports,
+//})
+
+const options = {
+    file: {
+        level: 'info',
+        filename: './logs/app.log',
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: false,
+    },
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+    },
+};
 
 const Logger = winston.createLogger({
-    level: level(),
-    levels,
-    format,
-    transports,
+    levels: winston.config.npm.levels,
+    transports: [
+        new winston.transports.File(options.file),
+        new winston.transports.Console(options.console)
+    ],
+    exitOnError: false
 })
 
 export default Logger
